0822ce90294ed7610117104a272e9293ee5d8e64
Alaanzr <Alaanzr@gmail.com>
2016-03-09 01:38:19 +0000

    Date converter extracted to separate service

--- public/services/amadeus.js
+++ public/services/amadeus.js
@@ -1,29 +1,6 @@
-app.factory('Amadeus', ['$http', 'Config', function($http, Config) {
+app.factory('Amadeus', ['$http', 'Config', 'dateConverter', function($http, Config, dateConverter) {
   var baseUrl = 'https://api.sandbox.amadeus.com/v1.2/flights/extensive-search?';
   var apikey;
-  var months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
-
-  function convertMonthToNum(month) {
-    return '0' + (months.indexOf(month) + 1);
-  }
-
-  function convertHTMLDate(htmlDate) {
-    date = htmlDate.toString().split(' ');
-    return date[3] + '-' + convertMonthToNum(date[1]);
-  }
 
 Config.query().then(function(configs) {
   apikey = configs.data;
@@ -38,7 +15,7 @@ Config.query().then(function(configs) {
           apikey: apikey,
           origin: vm.origin,
           destination: vm.destination,
-          departure_date: convertHTMLDate(vm.outboundDate)
+          departure_date: dateConverter.convertHTMLDate(vm.outboundDate)
         }
       });
     }
